9f3a727e5dc431db4542c0eb9ccbf1617fbc346c
Hy Huynh <hyxhuynh@gmail.com>
2018-10-02 16:39:32 -0700

    added feature for Minutes Away to countdown and Next Arrival to update as time passes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
-  // Initialize Firebase
-  var config = {
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyCkqKnotITX5rOXBMoUMowVyrvYOJqqTrc",
     authDomain: "trainscheduler-59ab5.firebaseapp.com",
     databaseURL: "https://trainscheduler-59ab5.firebaseio.com",
@@ -18,82 +18,103 @@ var trainFrequency = 0;
 
 //========================FUNCTIONS=========================
 
-firebase.database().ref().on('child_added',function(childSnapshot){
-    console.log(childSnapshot.val());
-    
-    trainName = childSnapshot.val().name;
-    trainDestination = childSnapshot.val().destination;
-    trainFirstDeparture = childSnapshot.val().time;
-    trainFrequency = childSnapshot.val().frequency;
+// Update current time
+function currentTime() {
+    var current = moment().format("LTS");
+    $("#currentTime").html(current);
+    setTimeout(currentTime, 1000);
+}
+  
+// Update current date
+function currentDate() {
+    var current = moment().format("L");
+    $("#currentDate").html(current);
+    setTimeout(currentDate, 1000);
+}
 
-    console.log('Train Name: '+trainName);
-    console.log('Destination: '+trainDestination);
-    console.log('Train First Departure: '+trainFirstDeparture);
-    console.log('Frequency: '+trainFrequency);
-    
-    // Update current time 
-    function currentTime() {
-        var current = moment().format('LTS');
-        $("#currentTime").html(current);
-        setTimeout(currentTime, 1000);
-    };
+// Update the latest information in the table using Firebase as user adds new information
+// Use moment.js for Next Arrival and Minutes Away
+function updateTable() {
+    firebase
+        .database()
+        .ref()
+        .on("child_added", function(childSnapshot) {
+        console.log(childSnapshot.val());
 
-    // Update current date
-    function currentDate() {
-        var current = moment().format('L');
-        $("#currentDate").html(current);
-        setTimeout(currentDate, 1000);
-    };
+        trainName = childSnapshot.val().name;
+        trainDestination = childSnapshot.val().destination;
+        trainFirstDeparture = childSnapshot.val().time;
+        trainFrequency = childSnapshot.val().frequency;
 
-    // Execute function for current date and time and update HTML element
-    currentDate();
-    currentTime();
+        console.log("Train Name: " + trainName);
+        console.log("Destination: " + trainDestination);
+        console.log("Train First Departure: " + trainFirstDeparture);
+        console.log("Frequency: " + trainFrequency);
 
-    // Example: First departure time is 14:00 and interval is 30 minutes
-    // Current time at the station is 14:40
-    // Time difference from the first departure time: 40min
-    // Time remainder(the amount of time (minutes) passed since the last train arrival) 
-    // Time remainder = time difference(40min) % frequency(30min) = 10min 
-    // Next train will be arriving in frequency(30min) - time remainder(10min) = 20min
-    
-    // Convert first departure time (push back 1 year to make sure it comes before current time)
-    var firstTimeConverted = moment(trainFirstDeparture, "HH:mm").subtract(1, "years");
-	console.log('First Time Converted: '+firstTimeConverted.format())
+        // Example: First departure time is 14:00 and interval is 30 minutes
+        // Current time at the station is 14:40
+        // Time difference from the first departure time: 40min
+        // Time remainder(the amount of time (minutes) passed since the last train arrival)
+        // Time remainder = time difference(40min) % frequency(30min) = 10min
+        // Next train will be arriving in frequency(30min) - time remainder(10min) = 20min
 
-	// Difference between the first departure time and the current time
-	var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-	console.log('Time Difference: '+diffTime)
+        // Convert first departure time (push back 1 year to make sure it comes before current time)
+        var firstTimeConverted = moment(trainFirstDeparture, "HH:mm").subtract(
+            1,
+            "years"
+        );
+        console.log("First Time Converted: " + firstTimeConverted.format());
 
-	// Time remainder (the amount of time (minutes) passed since the last train arrival)
-    var timeRemainder = diffTime % trainFrequency;
-    console.log('Time remainder: '+timeRemainder)
+        // Difference between the first departure time and the current time
+        var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+        console.log("Time Difference: " + diffTime);
 
-    // Minute until next train arrival
-    var minutesAway = trainFrequency - timeRemainder
-    console.log('Minutes Away: ' + minutesAway);
-	
-    // Next train arrival time
-    var nextArrival = moment().add(minutesAway, "minutes");
-    var nextArrivalConverted = moment(nextArrival).format("hh:mm a");
-    console.log('Next Arrival (unconverted): '+nextArrival);
-    console.log ('Next Arrival: '+nextArrivalConverted);
+        // Time remainder (the amount of time (minutes) passed since the last train arrival)
+        var timeRemainder = diffTime % trainFrequency;
+        console.log("Time remainder: " + timeRemainder);
 
-    var newRow = $("<tr>").append(
-        $("<td>").text(trainName),
-        $("<td>").text(trainDestination),
-        $("<td>").text(trainFrequency),
-        $("<td>").text(nextArrivalConverted),
-        $("<td>").text(minutesAway)
-    );
+        // Minute until next train arrival
+        var minutesAway = trainFrequency - timeRemainder;
+        console.log("Minutes Away: " + minutesAway);
 
-    // Append the new row to the table
-    $("#train-schedule > tbody").append(newRow);
-})
+        // Next train arrival time
+        var nextArrival = moment().add(minutesAway, "minutes");
+        var nextArrivalConverted = moment(nextArrival).format("hh:mm a");
+        console.log("Next Arrival (unconverted): " + nextArrival);
+        console.log("Next Arrival: " + nextArrivalConverted);
+
+        var newRow = $("<tr>").append(
+            $("<td>").text(trainName),
+            $("<td>").text(trainDestination),
+            $("<td>").text(trainFrequency),
+            $("<td>").text(nextArrivalConverted),
+            $("<td>").text(minutesAway)
+        );
+
+        // Append the new row to the table
+        $("#train-schedule > tbody").append(newRow);
+        });
+}
+
+// Execute function for current date and time and update HTML element
+currentDate();
+currentTime();
+
+// Execute updateTable function
+updateTable();
+
+// Update information in the table every 60 seconds 
+// This is to update Next Arrival and Minutes Away as time passes (moment.js) without reloading the page
+setInterval(function() {
+    $("tbody").empty();
+    updateTable();
+}, 60 * 1000);
 
 //========================EVENT LISTENERS=========================
 
 // When the submit button is clicked after filling out user's input
 $('#addTrain').on("click", function(){
+    event.preventDefault();
     trainName = $('#trainNameInput').val().trim();
     trainDestination = $('#destinationInput').val().trim();
     trainFirstDeparture = $('#firstDepartureInput').val().trim();
